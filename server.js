aef90e5530fdc4e33b19bbce304b75fd9677f603
Yash Garudkar <yashgarudkar@gmail.com>
2020-08-05 13:17:36 +0530

    REST API Complete

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-// Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+fastify.register(require('fastify-cors'))
 const PORT = 5000 || process.env.PORT
 
-// Run the server!
-fastify.listen(5000, (err, address)=>{
-  if (err){
+const stack = []
+//@Routes
+fastify.get('/', (request, reply) => {
+  reply.send(stack)
+})
+
+fastify.post('/addValue', (request, reply) => {
+  value = request.body.value
+  stack.push(value)
+  reply.send(stack)
+})
+
+fastify.put('/getLocation', (request, reply) => {
+  findValue = request.body.value
+  //check if value exists
+  if (stack.indexOf(findValue) === -1) {
+    reply.send('Err: No such element found! Please check values')
+  } else {
+    reply.send(stack.indexOf(findValue))
+  }
+})
+
+fastify.delete('/popElement', (request, reply) => {
+  stack.pop()
+  reply.send(stack)
+})
+
+//@Server!
+fastify.listen(5000, (err) => {
+  if (err) {
     console.log(err)
     process.exit(1)
   } else {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running, navigate to  https://localhost:${PORT}`)
   }
 })
